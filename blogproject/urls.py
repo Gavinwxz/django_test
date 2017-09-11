b21454d022063c9c3b59e2e1d1c87fb597f1ccc5
gavin <804572325@qq.com>
2017-09-11 20:30:43 +0800

    add url wechat app

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
+    url(r'^wechat/',include('wechat.urls')),
 ]
+
+
+
+
