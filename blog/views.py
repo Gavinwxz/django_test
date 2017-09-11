734775d4e031f4fbe744694a7b5a38d7ae4e4267
gavin <804572325@qq.com>
2017-09-11 16:41:45 +0800

    add webchat api test

--- blog/views.py
+++ blog/views.py
@@ -35,3 +35,27 @@ def detail(request,pk):
                 'comment_list':comment_list
                 }
     return render(request, 'blog/detail.html',context=context)
+
+
+def wexin(request):
+    """
+    所有的消息都会先进入这个函数进行处理，函数包含两个功能，
+    微信接入验证是GET方法，
+    微信正常的收发消息是用POST方法。
+    """
+    # 这个WEIXIN_TOKEN是在测试号的配置页面中配置的，等会会讲到
+    WEIXIN_TOKEN = 'xiaozhu110'
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = WEIXIN_TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("error")
